ee4031e7716125559d24b2bb398670e7ac936c78
JeeGAe <eleldk82@naver.com>
2023-08-11 15:21:44 +0900

    폴백 핸들러 추가

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get('/', (req, res, next) => {
   res.json({ code : 200 });
 })
 
+// 폴백 핸들러
+app.use( (req, res, next) => {
+  res.status(404).send("Server is not found")
+})
 
+app.use( (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Server error");
+})
 app.listen(3300, () => {
   console.log('Listening ...');
 })
